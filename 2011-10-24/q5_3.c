73edf649ad7afb867ad7b4a6310d4bb75962b7e0
kazuhiko Sakaguchi <pi8027@gmail.com>
2011-10-31 05:28:37 +0900

    refactoring

--- 2011-10-24/q5_3.c
+++ 2011-10-24/q5_3.c
@@ -2,8 +2,7 @@
 
 int main(void)
 {
-    int a[5][5] ,b[5][5] ,c[5][5];
-    int i, j, k;
+    int a[5][5] ,b[5][5], i, j, k, r;
     for(i = 0; i != 2; i++){
         for(j = 0; j != 5; j++){
             for(k = 0; k != 5; k++) scanf("%d", &(i ? b : a)[j][k]);
@@ -11,9 +10,9 @@ int main(void)
     }
     for(i = 0; i != 5; i++){
         for(j = 0; j != 5; j++){
-            c[i][j] = 0;
-            for(k = 0; k != 5; k++) c[i][j] += a[i][k] * b[k][j];
-            printf("%d ", c[i][j]);
+            r = 0;
+            for(k = 0; k != 5; k++) r += a[i][k] * b[k][j];
+            printf("%d ", r);
         }
         printf("\n");
     }
